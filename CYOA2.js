e957682b633f8bd3b8c66781acaf045f6d2319ef
test <session2@START-Coding-MBP-C02PLK6KG8WN.local>
2015-10-01 17:41:19 -0700

    Added the armor selection section and the confirm log in message to users.

--- CYOA2.js
+++ CYOA2.js
@@ -5,12 +5,12 @@
 /* ----------CALL VARIABLES---------- */
 var weapon = [];
 var armor = [];
-var userName = prompt("Welcome to Gargantua: The leading online virtual fantasy combat game. Please wait while we scan your face...Done! What is your name?");
+var userName = prompt("Welcome to Argantua: The leading online virtual fantasy combat game. Please wait while we scan your face...Done! What is your name?");
 /* ----------CALL VARIABLES---------- */
 
 //console.log(userName);
 
-var pickTheWeapon = prompt("Welcome, " + userName + ", you are about to get into the most advanced virtual world available. Because you're entering for the first time, you need to select your character's weapon. Pick one out of the 3: LONGSWORD, DUAL KNIVES, or MACE?");
+var pickTheWeapon = prompt("Welcome, " + userName + ", you are about to get into the most advanced virtual world available. Because you're entering for the first time, you need to select your character's weapon. Pick one out of the 3: LONGSWORD, DUAL KNIVES, or MACE?").toLowerCase();
 if(pickTheWeapon === "longsword"){
     var weapon = "longsword";
 }
@@ -24,4 +24,18 @@ else{
     var weapon = "fists";
 }
 
-var pickTheArmor = prompt("Now it's time to pick your armor. You have the choice ");
+var pickTheArmor = prompt("Now it's time to pick your armor. You have the choice between LIGHT cloth armor, MEDIUM leather armor, and HEAVY steel-plated armor.").toLowerCase();
+if(pickTheArmor === "light"){
+	var armor = "light armor";
+}
+else if (pickTheArmor === "medium") {
+	var armor = "medium armor";
+}
+else if (pickTheArmor === "heavy") {
+	var armor = "heavy armor";
+}
+else{
+	var armor = "bareskin";
+}
+
+var logIn = confirm("Done! Your character, " + userName + " has been completed! Are you ready to log into Argantua?");
